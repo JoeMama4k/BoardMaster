ad0ecddd34466062c7d6f283f95e47c0d9a9f302
Oriol <120737467+OriolAlonso@users.noreply.github.com>
2023-05-30 18:12:38 +0200

    Update tresenlinia.java

--- now/tresenlinia.java
+++ now/tresenlinia.java
@@ -130,3 +130,228 @@ public class TresEnRatlla extends Application {
         }
     }
 }
+
+
+
+///_________________________________________\\\
+package tasca2_metodes;
+
+import java.util.Scanner;
+
+/**
+ * (*) Escriu un programa per jugar al tres en ratlla.
+ * Dos jugadors, per torns, posen sobre un tauler de 3x3 un dels seus símbols, O
+ * o X.
+ * 
+ * Quan un jugador ha col·locat una fitxa de manera que té tres fitxes alineades
+ * verticalment, horitzontal o diagonal guanya la partida.
+ * 
+ * Si s'acaben les caselles buides i no s'ha arribat a cap alineament la partida
+ * acaba en empat
+ * 
+ * -------------
+ * | | | |
+ * -------------
+ * | | | |
+ * -------------
+ * | | | |
+ * -------------
+ * 
+ * Entra una fila (0, 1, or 2) pel jugador X: 1
+ * Entra una columna (0, 1, or 2) pel jugador X: 1
+ * 
+ * -------------
+ * | | | |
+ * -------------
+ * | | X | |
+ * -------------
+ * | | | |
+ * -------------
+ * 
+ * Entra una fila (0, 1, or 2) pel jugador O: 2
+ * Entra una columna (0, 1, or 2) pel jugador O: 1
+ * 
+ * -------------
+ * | | | |
+ * -------------
+ * | | X | O |
+ * -------------
+ * | | | |
+ * -------------
+ * 
+ * .......
+ * 
+ * -------------
+ * | X | | O |
+ * -------------
+ * | | X | O |
+ * -------------
+ * | | | X |
+ * -------------
+ * 
+ * Ha guanyat el jugador X en 3 tirades.
+ * 
+ * Fes l'exercici amb una filosofia modular. Descomposa el problema principal en
+ * problemes més petits. Per exemple podem tenir un mètode que pinti el tauler,
+ * o un altre que comprovi si la posició triada és correcte
+ * 
+ * Programa el necessari perquè no hi puguin haver errors a l'entrar les dades.
+ * Posa els missatges adequats perquè el jugador tingui clar què ha de fer en
+ * tot moment.
+ * 
+ * @author Joan Carcellé Obradors
+ * 
+ */
+
+public class Metodes3_3 {
+  static char[][] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+  static byte row = 0;
+  static  byte column = 0;
+  static final Scanner input = new Scanner(System.in);
+  static char currPlayer = 'O';
+  static boolean fullBoard = false;
+  static final String INPUTERRMSG = "Invalid input. Please enter two numbers between 0 and 2 with a space (e.g 2 0).";
+
+  public static void main(String[] args) {
+    System.out.println(
+        "This program is a Tic Tac Toe game where two players take turns making moves. The game is played on a 3x3 board. The player who gets three of their symbols in a row (horizontally, vertically or diagonally) first wins. If the board is full and no player has won, the game ends in a tie which is called a 'scratch'. The program prompts the current player to make a move by entering the row and column they would like to play in. It then checks if the move is valid (i.e. if the position is already occupied or if the input is outside the board) and if it is not, it prompts the player to make another move. The program continues until a player wins or the game ends in a tie, then displays the final state of the board and the outcome of the game.\n");
+    while (!checkWin()) {
+      changePlayer();
+      printBoard();
+      askPlay();
+    }
+    input.close();
+    printBoard();
+    System.out.println(fullBoard ? "Scratch!" : "Player " + currPlayer + " Wins");
+  }
+
+  // Splitted checkWin method into 2 methods to reduce the Cognitive Complexity
+
+  /**
+   * 
+   * Check if three characters are equal and not empty.
+   * 
+   * @param a the first character to compare.
+   * @param b the second character to compare.
+   * @param c the third character to compare.
+   * @return true if the three characters are equal and not empty, false
+   *         otherwise.
+   */
+  static boolean isWinningLine(char a, char b, char c) {
+    return a == b && b == c && a != ' ';
+  }
+
+  /**
+   * 
+   * Check if there is a win or a tie.
+   * 
+   * @return true if there is a win or a tie, false otherwise.
+   */
+  static boolean checkWin() {
+    // Check vertical alignment
+    for (int row = 0; row < 3; row++) {
+      // Check vertical alignment
+      if (isWinningLine(board[0][row], board[1][row], board[2][row])) {
+        return true;
+      }
+      // Check horizontal alignment
+      if (isWinningLine(board[row][0], board[row][1], board[row][2])) {
+        return true;
+      }
+    }
+
+    // Check diagonal alignment
+    if (isWinningLine(board[0][0], board[1][1], board[2][2])) {
+      return true;
+    }
+    if (isWinningLine(board[0][2], board[1][1], board[2][0])) {
+      return true;
+    }
+
+    // Check if the board is full for a tie
+    for (int row = 0; row < 3; row++) {
+      for (int column = 0; column < 3; column++) {
+        if (board[row][column] == ' ') {
+          return false;
+        }
+      }
+    }
+    fullBoard = true;
+    return true;
+  }
+
+  /**
+   * 
+   * Print the Tic Tac Toe board.
+   */
+  static void printBoard() {
+    System.out.println("-------------");
+    for (int i = 0; i < 3; i++) {
+      System.out.print("| ");
+      for (int j = 0; j < 3; j++) {
+        System.out.print(board[i][j] + " | ");
+      }
+      System.out.println();
+      System.out.println("-------------");
+    }
+  }
+
+  /**
+   * 
+   * Ask the current player for their move.
+   */
+  static void askPlay() {
+    
+    do {
+      System.out.print("Player " + currPlayer + " Make a Move (0, 1, or 2; e.g 0 1) : ");
+      if (getInput()) {
+        if (!isPosAlreadyUsed(row, column)) {
+          System.out.println("This position is already occupied. Choose another one.");
+        } else {
+          break;
+        }
+      }
+    } while (true);
+    board[row][column] = currPlayer;
+  }
+
+  /**
+   * Prompts the player for input and validates it.
+   * 
+   * @return {@code true} if the input of the user is valid, {@code false} otherwise.
+   */
+  static boolean getInput() {
+    if (input.hasNextByte()) {
+      row = input.nextByte();
+      if (input.hasNextByte()) {
+        column = input.nextByte();
+        if (row > 2 || row < 0 || column > 2 || column < 0) {
+          System.err.println(INPUTERRMSG);
+          return false;
+        }
+        return true;
+      }
+    }
+    System.err.println(INPUTERRMSG);
+    return false;
+  }
+
+  /**
+   * Change the current player.
+   */
+  static void changePlayer() {
+    currPlayer = (currPlayer == 'X' ? 'O' : 'X');
+  }
+
+  /**
+   * Check if the position is already used.
+   * 
+   * @param row    The row of the position
+   * @param column The column of the position
+   * @return `true` if the position is not used, `false` otherwise
+   */
+  static boolean isPosAlreadyUsed(byte row, byte column) {
+    return board[row][column] == ' ';
+  }
+}
+
